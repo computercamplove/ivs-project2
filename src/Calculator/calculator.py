e7b4f0bf4adabe81f3ca24fd15f82b95f2e89d86
computercamplove <jozefineme@gmail.com>
2021-03-26 02:26:20 +0100

    Add file calculator.py with logic for buttons

---
+++ src/Calculator/calculator.py
@@ -0,0 +1,76 @@
+from PyQt5 import QtWidgets
+from ui_calculator import Ui_Calculator
+import re
+
+class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+
+        #connect buttons
+        self.btn0.clicked.connect(self.number_pressed)
+        self.btn1.clicked.connect(self.number_pressed)
+        self.btn2.clicked.connect(self.number_pressed)
+        self.btn3.clicked.connect(self.number_pressed)
+        self.btn4.clicked.connect(self.number_pressed)
+        self.btn5.clicked.connect(self.number_pressed)
+        self.btn6.clicked.connect(self.number_pressed)
+        self.btn7.clicked.connect(self.number_pressed)
+        self.btn8.clicked.connect(self.number_pressed)
+        self.btn9.clicked.connect(self.number_pressed)
+
+        self.btn_dot.clicked.connect(self.decimal_pressed)
+        self.btn_plusminus.clicked.connect(self.unary_operator_pressed)
+        self.btn_clear.clicked.connect(self.clear_pressed)
+        self.btn_delete.clicked.connect(self.delete_pressed)
+
+        self.btn_left_b.clicked.connect(self.bracket_pressed)
+        self.btn_right_b.clicked.connect(self.bracket_pressed)
+
+    def decimal_pressed(self):
+        if '.' in self.display.text():
+            pass
+        else:
+            self.display.setText(self.display.text() + '.')
+
+    def number_pressed(self):
+        btn = self.sender()
+        result_label = format(float(self.display.text() + btn.text()), '.15g')
+
+        self.display.setText(result_label)
+
+    def unary_operator_pressed(self):
+        label_number = float(self.display.text())
+        label_number *= -1
+        result_label = format(label_number, '.15g')
+
+        self.display.setText(result_label)
+
+    def clear_pressed(self):
+        self.display.setText('0')
+
+    def delete_pressed(self):
+        label_number_str = self.display.text()
+
+        if len(label_number_str) == 1 or label_number_str == '0':
+            self.clear_pressed()
+        else:
+            result_label = label_number_str[:-1]
+            self.display.setText(result_label)
+
+    #TO-DO to finish after arithmetic methods
+    def bracket_pressed(self):
+        btn = self.sender()
+        label_number_str = self.display.text()
+
+        if btn.text() == '(':
+            if re.match(r"[\+\-\/\*]", label_number_str[-1]):
+                self.display.setText(self.display.text() + '(')
+        else: #button text is ")"
+            if not re.match(r"[\+\-\/\*]", label_number_str[-1]):
+                self.display.setText(self.display.text() + ')')
+
+
+
+

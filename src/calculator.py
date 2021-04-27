f8c0a912bb9d5c9821dc046d91020688f33de264
Zhamilya Abikenova <jozefineme@gmail.com>
2021-04-27 13:50:49 +0200

    Update calculator.py

--- src/calculator.py
+++ src/calculator.py
@@ -271,6 +271,8 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
         elif not self.lcd_string(lcd_str):
             #  display already contain 45 characters and returns previous input
             lcd_result = lcd_str
+        elif lcd_str[-1] in "0123456789":
+            lcd_result = lcd_str + '*('
         else:
             lcd_result = lcd_str + '('
 
